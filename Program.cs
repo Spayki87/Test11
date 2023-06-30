ddee34afd5be6ff4b88a073e6feb6b0bbe58aa7d
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-30 10:13:42 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,29 +12,24 @@ namespace Сумма_чисел
         {
             Random random = new Random();
 
-            //int number = random.Next(0, 101);
-            int number = 16;
+            int number = random.Next(0, 101);
             int multiples1 = 3;
             int multiples2 = 5;
             int sumMultiples1 = 0;
             int sumMultiples2 = 0;
-            int sumAllNumbers;
+            int sumAllNumbers = 0;
 
             Console.WriteLine("Рандомное число: " + number);
 
-            for (int i = 3; i < number; i++)
+            for (int i = 1; i <= number; i++)
             {
                 if (i % multiples1 == 0 || i % multiples2 == 0)
                 {
                     Console.WriteLine(i);
-                    sumMultiples1 += i;
-                    sumMultiples2 += i;
+                    sumAllNumbers  += i;
                 }
             }
-            sumAllNumbers = number + sumMultiples1 + sumMultiples2;
 
-            Console.WriteLine("Сумма чисел кратных " + multiples1 + ": " + sumMultiples1);
-            Console.WriteLine("Сумма чисел кратных " + multiples2 + ": " + sumMultiples2);
             Console.WriteLine("Сумма всех чисел: " + sumAllNumbers);
         }
     }
